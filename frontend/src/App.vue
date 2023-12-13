e46aeb6e15512baf50a3b3ff6ab198934d1392b6
coralinegrl <andrea.saldana@alumnos.ucn.cl>
2023-12-13 15:15:57 -0300

    RU 15:15

---
+++ frontend/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <ion-page>
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Mis Publicaciones</ion-title>
+      </ion-toolbar>
+    </ion-header>
+    <ion-content>
+      <!-- Componente de lista de publicaciones -->
+      <post-list></post-list>
+    </ion-content>
+  </ion-page>
+</template>
+
+<script>
+import { IonPage, IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/vue';
+import PostList from './components/PostList.vue';
+
+export default {
+  name: 'App',
+  components: {
+    IonPage,
+    IonHeader,
+    IonToolbar,
+    IonTitle,
+    IonContent,
+    PostList
+  }
+};
+</script>
+
+<style>
+/* Tus estilos personalizados aquí */
+</style>
